ea6097ab21fba367179ce438d8df12a1e78b127d
arki <antoniorafaelariasromero@gmail.com>
2025-03-12 10:10:56 -0500

    refactor: optimize frame capture loop

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,22 +1,15 @@
 import './style.css'
+import './reset.css'
 import startCamera from './camera';
 import captureFrame from './frame';
 
 const video = document.getElementById("video") as HTMLVideoElement;
 
+function captureLoop() {
+  captureFrame(video);
+  setTimeout(captureLoop, 115);
+}
+
 startCamera(video).then(() => {
-  setInterval(() => captureFrame(video), 30)
-})
-
-/**
-  * soon
-  * function captureLoop() {
-    captureFrame(video);
-    requestAnimationFrame(captureLoop);
-  }
-
-  * startCamera(video).then(() => {
-    requestAnimationFrame(captureLoop);
-  });
-
- */
+  captureLoop();
+});
